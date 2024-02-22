17a1631c1c44937496080249441bb35dcaefab73
Federico Mederos <fedemederos@MacBook-Pro-de-Federico.local>
2024-02-22 19:28:44 -0300

    todos los ejercicios OK

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,21 +1,12 @@
 const convertToCelsius = function(temp) {
-  if (temp < 0) {
-    celsius = Math.fround((((temp - 32) *5)/9));
+    celsius = Math.fround ((temp - 32) *5/9);
     return parseFloat(celsius.toFixed(1));
-  }else{  
-  celsius = Math.fround(((temp - 32) *5)/9);
-  return parseFloat(celsius.toFixed(1));
-}};
+};
 
 const convertToFahrenheit = function(temp) {
-  if (temp < 0) {
-    fahrenheit = Math.fround(((temp * 5/9)+32));
+    fahrenheit = Math.fround(temp * 9/5 + 32);
     return parseFloat(fahrenheit.toFixed(1));
-  }else{
-
-  fahrenheit = Math.fround(((temp * 5/9)+32));
-  return parseFloat(fahrenheit.toFixed(1));
-}};
+};
 
 // Do not edit below this line
 module.exports = {
